ae1e3b70cf39c576b45fbca16b2b764937f8769d
Alexandra <alexandracarvalho@ua.pt>
2021-03-26 11:26:17 +0000

    Completed integration tests

--- aula02/ex2/src/test/java/integration/AddressResolverIT.java
+++ aula02/ex2/src/test/java/integration/AddressResolverIT.java
@@ -12,6 +12,9 @@ import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AddressResolverIT {
 
@@ -22,17 +25,19 @@ public class AddressResolverIT {
 
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
-        //todo
+        TqsBasicHttpClient client = new TqsBasicHttpClient();
+        AddressResolver resolver = new AddressResolver(client);
 
-        //Address result = resolver.findAddressForLocation(40.640661, -8.656688);
-        //assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
-
+        Address result = resolver.findAddressForLocation(40.640661, -8.656688);
+        assertEquals( result, new Address( "Cais do Alboi", "Glória e Vera Cruz", "Centro", "3800-246", null) );
     }
 
     @Test
     public void whenBadCoordidates_trhowBadArrayindex() throws IOException, URISyntaxException, ParseException {
-        // todo
+        TqsBasicHttpClient client = new TqsBasicHttpClient();
+        AddressResolver resolver = new AddressResolver(client);
 
+        assertThrows(IndexOutOfBoundsException.class, () -> resolver.findAddressForLocation(100, 100));
     }
 
 }
